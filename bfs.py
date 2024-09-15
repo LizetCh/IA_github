fcf7c29fba6c3df2c4ddf512331aa49a4ea4439e
Liz <120131808+LizetCh@users.noreply.github.com>
2024-09-15 13:57:26 -0600

    Update bfs.py

--- bfs.py
+++ bfs.py
@@ -6,16 +6,27 @@ Algorithm to find the shortest path between two nodes
 from collections import deque
 
 def bfs_shortest_path(graph, start, end):
-		toVisit_queue = deque([(start, [start])]) #creates a double ended queue
-		while toVisit_queue: #while there's nodes to visit
-				(node, path) = toVisit_queue.popleft() #(node,path to node), FIFO 
-				for neighbor in graph[node]:
-						if neighbor not in path: #if not visited
-								if neighbor == end: #check if we've arrived
-										return path + [neighbor]
-								else:
-										toVisit_queue.append((neighbor, path + [neighbor]))
-		return None
+		
+	#check if start and end nodes are actually in the graphx
+	if start not in graph:
+		return f"{start} not in graph"
+	if end not in graph:
+		return f"{end} not in graph"
+
+
+	if start == end:
+		return [start]
+
+	toVisit_queue = deque([(start, [start])]) #creates a double ended queue
+	while toVisit_queue: #while there's nodes to visit
+		(node, path) = toVisit_queue.popleft() #(node,path to node), FIFO 
+		for neighbor in graph[node]:
+			if neighbor not in path: #if not visited
+				if neighbor == end: #check if we've arrived
+					return path + [neighbor]
+				else:
+					toVisit_queue.append((neighbor, path + [neighbor]))
+	return "No path found"
 
 graph = {
 		'Mérida': ['Hunucmá', 'Progreso', 'Motul','Kanasín', 'Umán'],
@@ -42,7 +53,7 @@ graph = {
 		'Tzucacab': ['Peto'],
 }
 
-start_node = 'Mérida'
-end_node = 'Peto'
+start_node = 'Pisté'
+end_node = 'Tzucacab'
 
 print(bfs_shortest_path(graph, start_node, end_node))
